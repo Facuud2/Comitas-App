9f2a4d739e5b9a5f054125bdfc3fd42d627f75df
Facuud2 <lunariush553@gmail.com>
2025-07-13 22:33:21 -0300

    Fix bad merge

--- js/reservas.js
+++ js/reservas.js
@@ -202,7 +202,7 @@
 //   });
 // });
 // reservas.js
-const API_URL = 'http://localhost:8080';
+const API_URL = 'https://comitas-app-backend.onrender.com';
 
 // Sucursales disponibles (podrían venir de una API real)
 const LOCALS = [
