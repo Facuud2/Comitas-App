f02d35e1efd8c376f89636aff15675a62a7fbd99
Facuud2 <lunariush553@gmail.com>
2025-07-22 21:54:47 -0300

    Fix: API_URL

--- js/reservas.js
+++ js/reservas.js
@@ -1,7 +1,7 @@
 
 
 // API_URL is loaded from env.js
-const API_URL = typeof ENV !== 'undefined' ? ENV.API_URL : undefined;
+const API_URL = process.env.API_URL;
 
 // Sucursales disponibles (podrían venir de una API real)
 const LOCALS = [
